1fbbf2bded7cd5314444072179bd351e772b72e4
maybb2003 <maybb2003@gmail.com>
2022-03-15 10:53:40 +0200

    feat:add ValidateRequest

--- users-crud - may - Copy/src/primes/primes.router.ts
+++ users-crud - may - Copy/src/primes/primes.router.ts
@@ -3,13 +3,13 @@ import { ValidateRequest } from '../utils/joi';
 import { wrapAsync } from '../utils/wrapper';
 import { PrimeController } from './primes.controller';
 import {
-    checkTheAmountSchema
+    checkTheAmountSchema, checkTheNumbersSchema
   } from './validator/primes.schema';
 
 
 const PrimeRouter: Router = Router();
 
-PrimeRouter.post('/prime/validate', wrapAsync(PrimeController.getNumbers));
+PrimeRouter.post('/prime/validate', ValidateRequest(checkTheNumbersSchema) ,wrapAsync(PrimeController.getNumbers)); 
 PrimeRouter.get('/prime', ValidateRequest(checkTheAmountSchema) , wrapAsync(PrimeController.getPrimeNumbers));
 
 export { PrimeRouter };
--- users-crud - may - Copy/src/primes/validator/primes.schema.ts
+++ users-crud - may - Copy/src/primes/validator/primes.schema.ts
@@ -1,43 +1,23 @@
 import * as Joi from 'joi';
 
-const PrimeSchema = Joi.object({
-  firstName: Joi.string(),
-  lastName: Joi.string(),
-  primename: Joi.string(),
-  email: Joi.string().regex(/^\S+@\S+\.\S+$/),
-  password: Joi.string(),
-});
+// const PrimeSchema = Joi.object({
+//   firstName: Joi.string(),
+//   lastName: Joi.string(),
+//   primename: Joi.string(),
+//   email: Joi.string().regex(/^\S+@\S+\.\S+$/),
+//   password: Joi.string(),
+// });
 
-const IdSchema = Joi.object({
-  id: Joi.string().length(24),
-});
+// const IdSchema = Joi.object({
+//   id: Joi.string().length(24),
+// });
 
 const amountSchema = Joi.object({
   amount: Joi.number().greater(1).less(32),
 });
 
-export const createPrimeReqSchema = Joi.object({
-  body: PrimeSchema,
-  query: {},
-  params: {},
-});
-
-export const getByIdReqSchema = Joi.object({
-  body: {},
-  query: {},
-  params: IdSchema,
-});
-
-export const getAllSchema = Joi.object({
-  body: {},
-  query: {},
-  params: {},
-});
-
-export const updatByIdSchema = Joi.object({
-  body: PrimeSchema,
-  query: {},
-  params: IdSchema,
+const numbersSchema = Joi.object({
+  numbers: Joi.array().items(Joi.string()),
 });
 
 export const checkTheAmountSchema = Joi.object({
@@ -45,5 +25,38 @@ export const checkTheAmountSchema = Joi.object({
   query: amountSchema,
   params: {},
 });
+
+export const checkTheNumbersSchema = Joi.object({
+  body: numbersSchema,
+  query: {},
+  params: {},
+});
+
+// export const createPrimeReqSchema = Joi.object({
+//   body: PrimeSchema,
+//   query: {},
+//   params: {},
+// });
+
+// export const getByIdReqSchema = Joi.object({
+//   body: {},
+//   query: {},
+//   params: IdSchema,
+// });
+
+// export const getAllSchema = Joi.object({
+//   body: {},
+//   query: {},
+//   params: {},
+// });
+
+// export const updatByIdSchema = Joi.object({
+//   body: PrimeSchema,
+//   query: {},
+//   params: IdSchema,
+// });
+
+
+
 
 
