afce6eb781fd327a33ab4a620a4217f96ede9a57
maybb2003 <maybb2003@gmail.com>
2022-04-18 15:48:07 +0300

    feat: A function cannot be an ancestor of itself

--- overlap task part 1/groups/src/groups/groups.repository.ts
+++ overlap task part 1/groups/src/groups/groups.repository.ts
@@ -1,3 +1,4 @@
+import { GroupNotFound } from '../utils/errors/group';
 import { Group } from './groups.interface';
 import { GroupModel } from './groups.model';
 
@@ -7,23 +8,38 @@ export class GroupRepository {
     return GroupModel.create(newGroup);
   }
 
-  static findGroupByID(groupId: string): Promise<Group[]>{
-    return GroupModel.find({groupId}).exec();
+  static findGroupByID(groupId: string): Promise<Group[]> {
+    return GroupModel.find({ groupId }).exec();
   }
 
   static updateGroupByID(groupId: string, postData: Partial<Group>): Promise<Group | null> {
-    return GroupModel.findByIdAndUpdate(groupId, postData, {upsert: true}).exec();
+    return GroupModel.findByIdAndUpdate(groupId, postData, { upsert: true }).exec();
   }
-  
+
   static deleteGroupByID(groupId: string): Promise<Group | null> {
     return GroupModel.findByIdAndDelete(groupId).exec();
   }
 
   static addPeron(groupId: string, personId: Partial<string>): Promise<Group | null> {
-    return GroupModel.findByIdAndUpdate(groupId, {$addToSet: { participants: personId } }, {new: true}).exec();
+    return GroupModel.findByIdAndUpdate(groupId, { $addToSet: { participants: personId } }, { new: true }).exec();
   }
 
-  static addSubgroup(mainGroupId: string, subgroupId: Partial<string>): Promise<Group | null> {
-    return GroupModel.findByIdAndUpdate(mainGroupId, {$addToSet: { subgroups: subgroupId } }, {new: true}).exec();
+  static async addSubgroup(mainGroupId: string, subgroupId: string): Promise<Group | null> {
+    const group = await GroupModel.findById(mainGroupId);
+    const flag = await GroupRepository.findPrimaryFunction(group, subgroupId);
+    if (flag) throw new GroupNotFound;
+    return GroupModel.findByIdAndUpdate(mainGroupId, { $addToSet: { subgroups: subgroupId } }, { new: true }).exec();
   }
+
+  static async findPrimaryFunction(group: any, newSubgroupId: string): Promise<boolean> {
+    const subgroups = await GroupModel.find({ $in: group.subgroups }).exec();
+    let flag = false; 
+    for (const subgroup of subgroups) {
+      if(subgroup.id === newSubgroupId) {
+        flag = true;
+    }
+    GroupRepository.findPrimaryFunction(subgroup, newSubgroupId);
+  }
+  return flag;
+ }
 }
